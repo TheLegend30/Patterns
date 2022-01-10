5ea9c87511456776fd5c8853cb6f9d8d10f4b623
Oleh <Luibichenko.Oleg@gmail.com>
2022-01-10 17:37:44 +0200

    Created new Adapter and Bridge patterns project

---
+++ src/structural/bridge/CppDeveloper.java
@@ -0,0 +1,8 @@
+package structural.bridge;
+
+public class CppDeveloper implements Developer{
+    @Override
+    public void writeCode() {
+        System.out.println("Cpp dev writes cpp code");
+    }
+}
